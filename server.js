743534c908c6e4920d5782f1dbee01fc431d551d
Anukritik007 <anukritik77@gmail.com>
2020-11-21 16:55:44 +0530

    connected to mongoose

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
-const express = require('express');
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
 const app = express();
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require("express-ejs-layouts");
 
-const indexRouter = require('./routes/index');
+const indexRouter = require("./routes/index");
 
-app.set('view engine','ejs'); //using ejs as view engine
-app.set('views',__dirname + '/views');
-app.set('layout','layouts/layout'); // every single file is going to be in layout(so that header & footer need not be duplicated)
+app.set("view engine", "ejs"); //using ejs as view engine
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout"); // every single file is going to be in layout(so that header & footer need not be duplicated)
 
 app.use(expressLayouts);
-app.use(express.static('public')); //files such as styles, js
-app.use('/',indexRouter);
+app.use(express.static("public")); //files such as styles, js
 
-app.listen(process.env.PORT || 3000);
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true
+});
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 4300);
